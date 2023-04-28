776de8281b1523aea645967bd81989b957ebdd16
AFzOfficial <abol.mortal50@gmail.com>
2023-04-29 00:24:49 +0330

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import openai, os, time, platform
+
+openai.api_key = '' # SET YOUR API KEY
+
+
+messages = []
+
+
+def Typing(text):
+    for char in text:
+        time.sleep(0.020)
+        print(char, end='', flush=True)
+
+
+
+def app():
+    while True:
+        promt = input('\n>>> ')
+
+        messages.append({"role": "user", "content": f"{promt}"})
+
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+            )
+
+            response = response.choices[0].message["content"]
+
+            messages.append({"role": "system", "content": f"{response}"})
+
+            print('\n\nGPT : ', end=' ')
+
+            Typing(response)
+
+            print('\n')
+
+        except Exception as e:
+            print(f'\nGPT : {e}\n')
+
+
+
+
+
+app()
