30ac56151b193efa4d12b04cb53be63a490286b7
AFzOfficial <abol.mortal50@gmail.com>
2023-09-29 00:00:09 +0330

    refactor: function names, some fixes

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import openai, os, time, platform
 
-openai.api_key = '' # SET YOUR API KEY
+openai.api_key = '' # set your API key
 
-
+# store messages in memmory
 messages = []
 
 
 
-def Typing(text):
+def type_animation(text):
+    '''
+    this function for typing animation in CLI
+    input: str
+    output: char
+    '''
+
     for char in text:
         time.sleep(0.020)
         print(char, end='', flush=True)
 
 
 
+def send_request(prompt):
+    '''
+    send requests to openai API
+    input: user prompt
+    output: system response
+    '''
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
 
-def app():
+    response = response.choices[0].message["content"]
+
+    return response
+
+
+
+
+
+def main():
     os.system('cls' if platform.system() == 'Windows' else 'clear')
     while True:
         prompt = input('\n>>> ')
@@ -23,26 +47,21 @@ def app():
         messages.append({"role": "user", "content": f"{prompt}"})
 
         try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages
-            )
-
-            response = response.choices[0].message["content"]
+            response = send_request(prompt)
 
             messages.append({"role": "system", "content": f"{response}"})
 
-            print('\n\nGPT : ', end=' ')
+            print('\n\nCHAT GPT : ', end=' ')
 
-            Typing(response)
+            type_animation(response)
 
             print('\n')
 
         except Exception as e:
-            print(f'\nGPT : {e}\n')
+            print(f'\nCHAT GPT : {e}\n')
 
 
 
 
             
-app()
+main()
